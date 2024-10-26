7cf621278aefded7a13608d64385b80bf9947784
Agbortoko <arreyagbortoko@gmail.com>
2024-10-26 14:40:34 +0100

    Corrections

---
+++ admin/templates/sidebar.php
@@ -0,0 +1,51 @@
+ <div id="layoutSidenav_nav">
+     <nav class="sb-sidenav accordion bg-white" id="sidenavAccordion">
+         <div class="sb-sidenav-menu">
+             <div class="nav">
+                 <div class="sb-sidenav-menu-heading">Core</div>
+                 <a class="nav-link text-black" href="index.html">
+                     <div class="sb-nav-link-icon">
+                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-badge-vr" viewBox="0 0 16 16">
+                             <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z" />
+                             <path d="M4.508 11h1.429l1.99-5.999H6.61L5.277 9.708H5.22L3.875 5.001H2.5zm6.387-5.999H8.5V11h1.173V8.763h1.064L11.787 11h1.327L11.91 8.583C12.455 8.373 13 7.779 13 6.9c0-1.147-.773-1.9-2.105-1.9zm-1.222 2.87V5.933h1.05c.63 0 1.05.347 1.05.989 0 .633-.408.95-1.067.95z" />
+                         </svg>
+                     </div>
+                     Dashboard
+                 </a>
+
+                 <a class="nav-link text-black" href="charts.html">
+                     <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                     Charts
+                 </a>
+                 <a class="nav-link text-black" href="tables.html">
+                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                     Tables
+                 </a>
+
+                 <a class="nav-link text-black" href="tables.html">
+                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                     Tables
+                 </a>
+
+                 <a class="nav-link text-black" href="tables.html">
+                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                     Tables
+                 </a>
+
+                 <a class="nav-link text-black" href="tables.html">
+                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                     Tables
+                 </a>
+
+                 <a class="nav-link text-black" href="tables.html">
+                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                     Tables
+                 </a>
+             </div>
+         </div>
+         <div class="sb-sidenav-footer">
+             <div class="small">Logged in as:</div>
+             Start Bootstrap
+         </div>
+     </nav>
+ </div>
