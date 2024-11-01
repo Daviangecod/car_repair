afd9c1e2eddce7d8e473904ed887a0889dcc3397
DataDavinci <davidaebenda@gmail.com>
2024-11-01 16:17:21 +0100

    worked on sidebar pages

--- admin/templates/navbar.php
+++ admin/templates/navbar.php
@@ -1,8 +1,17 @@
+<link rel="stylesheet" href="../asstes/css/styles.css">
 <nav class="sb-topnav navbar navbar-expand bg-white">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+            <a class="navbar-brand ps-3 logo" style= "color: #1e2130; font-size: 3.5rem; font-weight: bold;" href="index.php">D-cars</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm text-black order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- notification icon -->
+             <div class="icon" style="margin-left: 700px">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#31364d" fill="none">
+                <path d="M5.15837 11.491C5.08489 12.887 5.16936 14.373 3.92213 15.3084C3.34164 15.7438 3 16.427 3 17.1527C3 18.1508 3.7818 19 4.8 19H19.2C20.2182 19 21 18.1508 21 17.1527C21 16.427 20.6584 15.7438 20.0779 15.3084C18.8306 14.373 18.9151 12.887 18.8416 11.491C18.6501 7.85223 15.6438 5 12 5C8.35617 5 5.34988 7.85222 5.15837 11.491Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M10.5 3.125C10.5 3.95343 11.1716 5 12 5C12.8284 5 13.5 3.95343 13.5 3.125C13.5 2.29657 12.8284 2 12 2C11.1716 2 10.5 2.29657 10.5 3.125Z" stroke="currentColor" stroke-width="1.5" />
+                <path d="M15 19C15 20.6569 13.6569 22 12 22C10.3431 22 9 20.6569 9 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            </div>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div class="input-group">
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,11 +10,12 @@
     <link rel="stylesheet" href="bootstrap-5.2.3-dist/css/bootstrap.css">
     <title>Home page</title>
 </head>
-<body class="bg-image">
-    <?php require 'template/navbar.php';?> 
 
-<!-- Texts -->
-<div id="outer" class="text">
+<body class="bg-image">
+    <?php require 'template/navbar.php'; ?>
+
+    <!-- Texts -->
+    <div id="outer" class="text">
         <div id="inner" class="row">
             <div class="col-12">
                 <p class="responsive-text">Find car repair shops near you</p>
@@ -21,28 +23,29 @@
             </div>
         </div>
     </div>
-<!-- Texts -->
+    <!-- Texts -->
 
-<!-- search bar -->
- <form action="">
+    <!-- search bar -->
+    <form action="">
 
-<div class="search">
+        <div class="search">
 
-    <div class="row height d-flex justify-content-center align-items-center">
+            <div class="row height d-flex justify-content-center align-items-center">
 
-        <div class="col-md-8">
+                <div class="col-md-8">
 
-            <div class="search">
-                <i class="fa fa-search" aria-hidden="true"></i>
-                <input type="text" class="form-control" placeholder="Find a shop">
-                <button class="">Search</button>
+                    <div class="search">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                        <input type="text" class="form-control" placeholder="Find a shop">
+                        <button class="">Search</button>
+                    </div>
+
+                </div>
+
             </div>
-    
         </div>
-  
-    </div>
-</div>
 
-</form>
+    </form>
 </body>
+
 </html>
