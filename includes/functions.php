ceb6e6f4d79d59665a3c4d931d43f496a4320ff5
Agbortoko <arreyagbortoko@gmail.com>
2024-11-04 20:15:56 +0100

    Setup base files for project

---
+++ includes/functions.php
@@ -0,0 +1,51 @@
+<?php 
+
+require_once __DIR__ . "/../config/settings.php";
+
+if (!function_exists("baseUrl")) {
+    function baseUrl(string $path = null, array $query = null)
+    {
+
+        if ($path !== null && $query !== null) {
+            $build = http_build_query($query);
+            return getConfig('base_url') . "/" . $path . "?" . $build;
+        } elseif ($path !== null) {
+            return getConfig('base_url') . "/" . $path;
+        }
+
+        return getConfig('base_url');
+    }
+}
+
+if (!function_exists("resourceUrl")) {
+    function resourceUrl(string $path = null)
+    {
+        if ($path !== null) {
+            return baseUrl('resources' .  "/" . $path);
+        }
+        return baseUrl('resources');
+    }
+}
+
+
+if (!function_exists("basePath")) {
+    function basePath(string $path = null)
+    {
+        if ($path !== null) {
+            return getConfig('base_path') . "/" . $path;
+        }
+        return getConfig('base_path');
+    }
+}
+
+
+if (!function_exists("redirect")) {
+    function redirect(string $to, array $query = null)
+    {
+        if ($query !== null) {
+            $httpQuery = http_build_query($query);
+            return header("Location: $to?$httpQuery");
+        }
+        return header("Location: $to");
+    }
+}
