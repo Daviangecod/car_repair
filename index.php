73596382c4de9242baf7b0907d47986ce0f2eb0b
Agbortoko <arreyagbortoko@gmail.com>
2024-11-04 21:20:58 +0100

    Added bootstrap 5 to the project

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php 
+<?php require "./vendor/autoload.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="<?= baseUrl('assets/vendor/bootstrap/css/bootstrap.min.css') ?>">
+</head>
+<body>
+
+    
+    <script src="<?= baseUrl('assets/vendor/bootstrap/js/bootstrap.min.js') ?>"></script>
+</body>
+</html>
