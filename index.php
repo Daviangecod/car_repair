c0ff8bd49d9a3ea7f0a18bef8ce7e63477f5133c
Agbortoko <arreyagbortoko@gmail.com>
2024-11-14 10:45:06 +0100

    Added a simple footer on landing page

--- index.php
+++ index.php
@@ -62,6 +62,14 @@
 
     </main>
 
+    <footer class="py-3 bg-theme-primary">
+
+        <div class="container">
+                <p class="text-center text-white mb-0">Copyright &copy; <?= date('Y') ?> D-Cars</p>
+        </div>
+
+    </footer>
+
     <script src="<?= baseUrl('assets/vendor/bootstrap/js/bootstrap.min.js') ?>"></script>
 </body>
 
