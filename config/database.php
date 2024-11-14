3944c0d827b7a67dc3374821e51bffce3f128414
Agbortoko <arreyagbortoko@gmail.com>
2024-11-14 10:41:53 +0100

    Designed the homepage

--- config/database.php
+++ config/database.php
@@ -1,26 +1,21 @@
 <?php
 
-function dbConnect()
-{
-    $dbConfig = [
-        'host' => 'localhost',
-        'username' => 'root',     
-        'password' => '',     
-        'database' => 'eams',   
-    ];    
+$dbConfig = [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'eams',
+];
 
-    // Create a connection using MySQLi (Procedural)
-    $connection = mysqli_connect(
-        $dbConfig['host'],
-        $dbConfig['username'],
-        $dbConfig['password'],
-        $dbConfig['database']
-    );
+// Create a connection using MySQLi (Procedural)
+$connection = mysqli_connect(
+    $dbConfig['host'],
+    $dbConfig['username'],
+    $dbConfig['password'],
+    $dbConfig['database']
+);
 
-    // Check the connection
-    if (!$connection) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-
-    return $connection;
+// Check the connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
 }
--- config/settings.php
+++ config/settings.php
@@ -4,24 +4,11 @@ if(!function_exists("getConfig")){
 
     function getConfig($key) {
 
-        global $connection;
-        require_once __DIR__ . '/database.php';
-
-        $connection = dbConnect();
-
-        $query = "SELECT * FROM settings";
-        $result = mysqli_query($connection, $query);
-        
-        if(mysqli_num_rows($result) > 0) {
-
-            $settings = mysqli_fetch_all($result);
-
-            $siteConfig = [
-                'base_url' => "http://localhost/car_repair",     
-                'base_path' => dirname(__DIR__, 1)
-            ];
-        }
-        
+        $siteConfig = [
+            'base_url' => "http://localhost/car_repair",     
+            'base_path' => dirname(__DIR__, 1)
+        ];
+                
         return $siteConfig[$key] ?? null; 
 
     }
